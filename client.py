d11fb91f8af560fed9335ab2c2c3f7501d95e754
Nick Jackson <nick@nickjackson.me>
2016-03-09 15:31:58 +0000

    Betterment!

--- client.py
+++ client.py
@@ -1,4 +1,9 @@
-import sys, yaml, time
+import sys
+import yaml
+import time
+import Tkinter
+import threading
+
 sys.path.append('gen-py')
 
 from baltimore import Baltimore
@@ -8,50 +13,164 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from Tkinter import *
+
+
+def callback():
+    print "click!"
+
+
+class heartbeatThread(threading.Thread):
+    def __init__(self, name='heartbeatThread'):
+        self._stopevent = threading.Event()
+        self._sleepperiod = 3.0
+        threading.Thread.__init__(self, name=name)
+
+    def run(self):
+        while not self._stopevent.isSet():
+            try:
+                client.heartbeat()
+                print 'Heartbeat: Server alive.'
+                connectionStatusString.set('Connected')
+
+            except Thrift.TException, tx:
+                connectionStatusString.set('Not Connected')
+                print 'Heartbeat: Unable to reach server.'
+
+            root.update_idletasks()
+
+            self._stopevent.wait(self._sleepperiod)
+
+    def join(self, timeout=None):
+        self._stopevent.set()
+        threading.Thread.join(self, timeout)
+
+
+def ampOn():
+    client.amplifierOn()
+    print 'Powering up amplifier.'
+
+
+def ampOff():
+    client.amplifierOff()
+    print 'Powering down amplifier.'
+
+
+def chimesOn():
+    client.hourChimeOn()
+    print 'Switching hourly chimes on.'
+
+
+def chimesOff():
+    client.hourChimeOff()
+    print 'Switching hourly chimes off.'
+
+
+def runTest():
+    client.test()
+    print 'Playing test audio.'
+
+
+def playThreePeal():
+    client.play('test.wav')
+    print 'Playing three peal bells.'
+
+
+def playWeddingPeal():
+    client.play('test.wav')
+    print 'Playing wedding bells.'
+
+
+def refreshStatus():
+    ampState = client.amplifierState()
+    chimeState = client.hourChimeState()
+    ampStatusString.set('Amp On' if ampState else 'Amp Off')
+    chimesStatusString.set('Chimes On' if chimeState else 'Chimes Off')
+    root.update_idletasks()
+    print 'Updating server status.'
+
 # Load up the config
 with open("config.yml", 'r') as configFile:
     conf = yaml.load(configFile)
 
-try:
+connectedFlag = False
 
-    # Make socket
-    transport = TSocket.TSocket(conf['server'], 9090)
+while (not connectedFlag):
 
-    # Buffering is critical. Raw sockets are very slow
-    transport = TTransport.TBufferedTransport(transport)
+    try:
 
-    # Wrap in a protocol
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        # Make socket
+        transport = TSocket.TSocket(conf['server'], 9090)
 
-    # Create a client to use the protocol encoder
-    client = Baltimore.Client(protocol)
+        # Buffering is critical. Raw sockets are very slow
+        transport = TTransport.TBufferedTransport(transport)
 
-    # Connect!
-    transport.open()
+        # Wrap in a protocol
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
-    client.heartbeat()
-    print 'Sent heartbeat.'
+        # Create a client to use the protocol encoder
+        client = Baltimore.Client(protocol)
 
-    amplifierState = client.amplifierState()
-    print 'Amplifier is: ' + ('On' if amplifierState else 'Off')
+        # Connect!
+        transport.open()
 
-    hourChimeState = client.hourChimeState()
-    print 'Hourly chimes are: ' + ('On' if hourChimeState else 'Off')
+        # Hooray!
+        print 'Connected to server.'
+        connectedFlag = True
 
-    while True:
+    except Thrift.TException, tx:
+        print '%s' % (tx.message)
+        print 'Retrying in 10 seconds.'
+        time.sleep(10)
 
-        fileName = raw_input("What to play?")
+# Made it here? We're connected! Awesome!
 
-        # BELLS!
-        client.play(fileName)
+# Initialise the window root so we can line up some status strings
 
-        raw_input("Stop?")
+root = Tkinter.Tk()
+connectionStatusString = StringVar()
+ampStatusString = StringVar()
+chimesStatusString = StringVar()
 
-        # Stop!
-        client.stop()
+# Fire off the heartbeat thread.
 
-    # Close!
-    transport.close()
+heartbeat = heartbeatThread()
+heartbeat.start()
 
-except Thrift.TException, tx:
-    print '%s' % (tx.message)
+connectionStatusLabel = Label(root, textvariable=connectionStatusString)
+connectionStatusLabel.pack()
+
+ampStatusLabel = Label(root, textvariable=ampStatusString)
+ampStatusLabel.pack()
+
+chimesStatusLabel = Label(root, textvariable=chimesStatusString)
+chimesStatusLabel.pack()
+
+ampOnButton = Button(root, text="Amp On", command=ampOn)
+ampOnButton.pack()
+
+ampOffButton = Button(root, text="Amp Off", command=ampOff)
+ampOffButton.pack()
+
+chimesOnButton = Button(root, text="Chimes On", command=chimesOn)
+chimesOnButton.pack()
+
+chimesOffButton = Button(root, text="Chimes Off", command=chimesOff)
+chimesOffButton.pack()
+
+ampRefreshButton = Button(root, text="Refresh Status", command=refreshStatus)
+ampRefreshButton.pack()
+
+testButton = Button(root, text="Test", command=runTest)
+testButton.pack()
+
+threePealButton = Button(root, text="Bells: Three Peal", command=playThreePeal)
+threePealButton.pack()
+
+weddingPealButton = Button(root, text="Bells: Wedding", command=playWeddingPeal)
+weddingPealButton.pack()
+
+# Get the status of things before we start
+refreshStatus()
+
+root.mainloop()
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-import sys, pygame
+import sys
+import pygame
+import time
+import threading
+
 sys.path.append('gen-py')
 
 from baltimore import Baltimore
@@ -8,9 +12,62 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
-from threading import Thread
+from datetime import datetime
+
+# Variables!
+fadeoutDelay = 3
+heartbeatSilenceCount = 0
+clientConnected = False
+lastHourChimed = None
+
+# A class to control the amplifier
+
+
+class AmplifierControl:
+
+    amplifierPower = False
+
+    def on(self):
+        print 'Powering up amplifier.'
+        # WORK
+        self.amplifierPower = True
+
+    def off(self):
+        global hourChime
+        print 'Beginning amplifier power-down sequence.'
+        print 'Disabling hourly chimes.'
+        hourChime = False
+        print 'Stopping audio.'
+        mixer.stop()
+        time.sleep(fadeoutDelay)
+        print 'Powering down amplifier.'
+        # WORK
+        self.amplifierPower = False
+        print 'Amplifier off.'
+
+# A class to handle audio output
+
+
+class AudioMixer:
+
+    def __init__(self):
+        pygame.mixer.init()
+
+    def playTest(self):
+        print 'Playing test audio file.'
+        self.play('test/test.wav')
+
+    def play(self, filename, loops = 0):
+        print 'Playing audio file: ' + filename
+        pygame.mixer.music.load(filename)
+        pygame.mixer.music.play(loops)
+
+    def stop(self):
+        print 'Stopping current audio.'
+        pygame.mixer.music.fadeout(fadeoutDelay*1000)
 
 # This is where we respond to things.
+
 
 class BaltimoreHandler:
 
@@ -18,42 +75,103 @@ class BaltimoreHandler:
         self.log = {}
 
     def heartbeat(self):
+        global clientConnected, heartbeatSilenceCount
+        heartbeatSilenceCount = 0
+        clientConnected = True
         print 'Heartbeat received from client!'
 
+    def amplifierOff(self):
+        global amplifier
+        amplifier.off()
+
+    def amplifierOn(self):
+        global amplifier
+        amplifier.on()
+
+    def hourChimeOff(self):
+        global hourChime
+        hourChime = False
+
+    def hourChimeOn(self):
+        global hourChime
+        hourChime = True
+
     def amplifierState(self):
-        print 'Amplifier state request. Replied with: ' + ('On' if amplifierPower else 'Off')
-        return amplifierPower
+        global amplifier
+        print 'Amplifier state request. Replied with: ' + ('On' if amplifier.amplifierPower else 'Off')
+        return amplifier.amplifierPower
 
     def hourChimeState(self):
+        global hourChime
         print 'Hourly chime state request. Replied with: ' + ('On' if hourChime else 'Off')
         return hourChime
 
     # Load and play the given filename
     def play(self, filename):
-        print 'Playing audio file: ' + filename
-        pygame.mixer.music.load('audio/' + filename)
-        pygame.mixer.music.play(-1)
+        global mixer
+        mixer.play('audio/' + filename, -1)
 
     # Fade the audio out.
     def stop(self):
-        print 'Stopping current audio.'
-        pygame.mixer.music.fadeout(3000)
+        global mixer
+        mixer.stop()
 
     # Load and play the test files
     def test(self):
-        print 'Playing test audio file.'
-        pygame.mixer.music.load('test/test.wav')
-        pygame.mixer.music.play()
+        global mixer
+        mixer.playTest()
 
 
-# Here are our threads!
+class tickTockThread(threading.Thread):
+    def __init__(self, name='heartbeatThread'):
+        self._stopevent = threading.Event()
+        self._sleepperiod = 1.0
+        threading.Thread.__init__(self, name=name)
 
-# This is the server thread
+    def run(self):
+        global heartbeatSilenceCount, clientConnected, hourChime, lastHourChimed
+        while not self._stopevent.isSet():
 
-def serverThread():
-    print 'Server initialising...'
-    server = thriftServer.serve()
-    server.stop()
+            # Increment the heartbeat silence counter
+            heartbeatSilenceCount = heartbeatSilenceCount + 1
+
+            # Make sure there's a client connected. If not, gracefully kill things.
+            if (heartbeatSilenceCount > 10 and clientConnected):
+                print 'Client has disappeared! Powering down amplifier.'
+                amplifier.off()
+                clientConnected = False
+
+            # Hourly bells!
+
+            if hourChime:
+
+                # Is it the top of the hour, and not the last chimed hour?
+                if (datetime.now().minute is 00 and
+                        datetime.now().hour is not lastHourChimed):
+                    hour = datetime.now().hour
+
+                    # Record that we've done this hour, or we'll be bonging all over the shop.
+                    lastHourChimed = hour
+
+                    # Calculate the necessary bonging.
+
+                    # Hours come in 24 flavour!
+                    if hour >= 12:
+                        hour = hour - 12
+
+                    # If the hour is 0 it's either midnight, or noon. Either way it's 12 bongs!
+                    if hour is 0:
+                        hour = 12
+
+                    print 'Chiming the hour! Number of bongs: ' + str(hour)
+
+                    mixer.play('chimes/bong.wav', hour - 1)
+
+            self._stopevent.wait(self._sleepperiod)
+
+    def join(self, timeout=None):
+        self._stopevent.set()
+        threading.Thread.join(self, timeout)
 
 # Initialise all the Thrift stuff
 
@@ -67,23 +185,20 @@ thriftServer = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
 
 # Off we go!
 
-# This is so the loop knows to keep going
-serverAlive = True;
-
 # This should be the default state of affairs.
-hourChime = False;
-amplifierPower = False;
+hourChime = False
 
 print 'Good morning Baltimore!'
 
-# Initialise the mixer.
-pygame.mixer.init()
+# Initialise our tasty objects!
+amplifier = AmplifierControl()
+mixer = AudioMixer()
 
-# Begin threads!
-# server = Thread(target = serverThread)
+# Kick off the tick tock thread!
+tickTock = tickTockThread()
+tickTock.start()
 
-# server.start();
-
+# Tell Thrift to start listening in
 thriftServer.serve()
 
 print 'Goodbye.'
